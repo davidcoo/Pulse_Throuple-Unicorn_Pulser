85a9d82edc92d55a8d241c84153a65e12b23c5cc
Nish <anilakan@andrew.cmu.edu>
2022-11-25 23:39:37 -0500

    Added proxy communication

---
+++ PiCode/proxy_receiver/receiver.c
@@ -0,0 +1,89 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include "pthread.h"
+#include <inttypes.h>
+
+#include "state.h"
+
+/** Configure this **/
+//#define LOCAL_HOST "128.2.57.96" // IP of local interface
+#define LOCAL_HOST "172.20.10.2" // IP of local interface
+#define R_PORT 13606
+
+//#define REMOTE_HOST "172.26.72.159"
+#define REMOTE_HOST "172.20.10.3"
+#define S_PORT 1000
+/** **/
+
+
+#define TX_INTERVAL_MS 300
+#define STATE_SIZE sizeof(DIJOYSTATE2_t)
+
+void *send_force(void *arg) {
+  int8_t force = 0;
+  int sockfd;
+  struct sockaddr_in servaddr;
+
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(S_PORT);
+  servaddr.sin_addr.s_addr = inet_addr(REMOTE_HOST);
+  
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("failed to create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    usleep(TX_INTERVAL_MS * 1000);
+    printf("Send force %d\n", force);
+    force += 5;
+    sendto(sockfd, (char*) &force, 1, MSG_CONFIRM,
+		    (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+  }
+}
+
+int main() {
+  int sockfd;
+  char buffer[STATE_SIZE + 1];
+
+  struct sockaddr_in servaddr = { 0 };
+
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("failed to create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(R_PORT);
+  servaddr.sin_addr.s_addr = inet_addr(LOCAL_HOST);
+
+  if (bind(sockfd, (const struct sockaddr *) &servaddr, 
+            sizeof(servaddr)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  DIJOYSTATE2_t state;
+  char recvbuf[sizeof(DIJOYSTATE2_t) + 4];
+  pthread_t send_tid;
+  pthread_create(&send_tid, NULL, send_force, NULL);
+
+  while(1) {
+    //printf("Wait recv\n");
+    int n, len;
+    n = recvfrom(sockfd, recvbuf, STATE_SIZE, MSG_WAITALL,
+                  (struct sockaddr *) &servaddr, &len);
+    uint32_t packet_ct = ((uint32_t*) recvbuf)[0];
+    memcpy(&state, recvbuf + 4, sizeof(state));
+    printf("Receive state (Pkt: %8X) :  Wheel: %d | Throttle: %d | Brake: %d\n", packet_ct, state.lX, state.lY, state.lRz);
+  }
+
+  return 0;
+}
